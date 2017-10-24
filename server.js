c60c3f0de0ef7aace872ff717f863b6cc1dee3b8
Will McAuliff <womcauliff@gmail.com>
2017-10-24 18:57:15 -0400

    Adds homepage route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Declares Express dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 // Configures Express server
 var app = express();
@@ -12,7 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
-
+//html routes
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname + "/index.html"));
+});
 
 // Makes public a static directory
 app.use(express.static(process.cwd() + '/public'));
